e97385e0d28724b7cf0883bfa9ed24b6f1bfca3f
MuryanBKS <109053834+MuryanBKS@users.noreply.github.com>
2024-03-01 10:40:39 +0800

    map

--- scenes/traps/spike_trap.gd
+++ scenes/traps/spike_trap.gd
@@ -1,9 +1,6 @@
 extends Area2D
 
-func trigger_trap():
-	$AnimatedSprite2D.play("default")
 
 
-func _on_area_entered(area: Area2D) -> void:
-	if area.get_parent() is Player:
-		print("hurt")
+func _on_body_entered(body: Node2D) -> void:
+	$AnimationPlayer.play("trigger")

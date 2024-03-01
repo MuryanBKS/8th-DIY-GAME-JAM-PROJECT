95d0b808b3f4b09b5ba859bb5454fffdb54330b1
MuryanBKS <109053834+MuryanBKS@users.noreply.github.com>
2024-03-01 11:59:43 +0800

    chest and key

--- scenes/objects/key.gd
+++ scenes/objects/key.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-
+var is_active = false
 
 
 func _on_body_entered(body: Node2D) -> void:
-	GameManager.get_key.emit()
-	queue_free()
+	if is_active:
+		GameManager.get_key.emit()
+		queue_free()
+
+func active():
+	is_active = true
+	$CollisionShape2D.set_deferred("disabled", false)
